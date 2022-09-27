fd71c622a30c3518bc7875c57f72638cf48f833d
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-09-27 12:51:01 -0500

    useState

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -1,6 +1,7 @@
 import { Card, CardImg, CardText, CardBody, Col } from 'reactstrap';
 
 const CampsiteDetail = ( {campsite} ) => {
+    const { image, name, description } = campsite;
 
     return (
         <Col md='12' className='m-4'>
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,7 +1,7 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
-export const selectRandomCampsites = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
 
 export const selectAllCampsites = () => {
