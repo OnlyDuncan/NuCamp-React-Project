ff645b545c7a61af8f382ff670fbd9ff107f1e06
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-09-28 13:16:54 -0500

    CampsiteDetailPage with UseParams

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,7 +1,7 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectAllCampsites = () => {
