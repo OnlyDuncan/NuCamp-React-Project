0e33a78555c4cc8bcff3f677fa6cc380cab9e483
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-10-11 13:09:50 -0500

    Add Middleware

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -2,20 +2,27 @@ import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import validateCommentForm from '../../utils/validateCommentForm';
+import { useDispatch } from 'react-redux';
+import { addComment } from './commentsSlice';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+
     const handleSubmit = (values) => {
-        const comment = 
-        {
+        const comment = {
             campsiteId: parseInt(campsiteId),
             rating: values.rating,
             author: values.author,
-            text: values.commentText
-        }
-        console.log(comment);
+            text: values.commentText,
+            date: new Date(Date.now()).toISOString()
+        };
+        console.log('comment:', comment);
+        dispatch(addComment(comment));
         setModalOpen(false);
-    }
+    };
+
     return (
         <>
             <Button outline onClick={() => setModalOpen(true)}>
